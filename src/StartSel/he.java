7580dcd33aafc84809ccb1453a4955be030d7650
santhoshMAX11 <santhosh.maxman@gmail.com>
2025-07-17 19:43:29 +0530

    final commit

---
+++ src/StartSel/he.java
@@ -0,0 +1,20 @@
+package StartSel;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class he {
+
+	
+	WebDriver driver = new ChromeDriver();
+	WebDriverWait wait =new  WebDriverWait(driver, Duration.ofSeconds(5));
+	//driver.manage().window().maximize();
+	
+}
